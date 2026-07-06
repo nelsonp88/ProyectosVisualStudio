2ff61892673a068f29b61907bf352df07887c5f3
nelsonp88 <nelsonp88@gmail.com>
2026-07-05 19:27:18 -0500

    Se reorganizan las carpetas de las aplicaciones

---
+++ TicketSystem/TicketApp/Models/TicketViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketApp.Models
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Código")]
+        public string TicketCode { get; set; } = string.Empty;
+
+        public int EventId { get; set; }
+
+        [Display(Name = "Evento")]
+        public string EventName { get; set; } = string.Empty;
+
+        //public Event? Event { get; set; }
+
+        public IEnumerable<SelectListItem> EventItems { get; set; }
+
+        public int? ReservationId { get; set; }
+
+        [Display(Name = "Código de reserva")]
+        public string ReservationCode { get; set; } = string.Empty;
+
+        //public Reservation? Reservation { get; set; }
+
+        public IEnumerable<SelectListItem> ReservationItems { get; set; }
+
+        [Display(Name = "Estado")]
+        public TicketStatus Status { get; set; } = TicketStatus.Available;
+
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "No. Silla")]
+        public string SeatNumber { get; set; } = string.Empty;
+
+        [Display(Name = "Fecha de creación")]
+
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public enum TicketStatus
+    {
+        [Display(Name = "Disponible")]
+        Available = 1,
+        [Display(Name = "Reservada")]
+        Reserved = 2,
+        [Display(Name = "Vendida")]
+        Sold = 3,
+        [Display(Name = "Cancelada")]
+        Cancelled = 4
+    }
+}
